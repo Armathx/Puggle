2a66085137def5a1ba44b1d494d6ebd96e10a725
Thomas Buffet <116625979+Armathx@users.noreply.github.com>
2025-01-14 21:30:42 +0100

    UpdateBalls

--- Assets/Scripts/Balls.cs
+++ Assets/Scripts/Balls.cs
@@ -7,58 +7,57 @@ public class Balls : MonoBehaviour
 {
 
     public GameObject BallsPrefabs;
-    private Vector2 mouseOffset;
-    private bool isDragging = false;
-    private BoxCollider2D boxCollider;
+    //private Vector2 mouseOffset;
+    //private bool isDragging = false;
+    //private BoxCollider2D boxCollider;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Taille de l'écran en unités du monde.
-        float screenWidth = Camera.main.orthographicSize * 2 * Camera.main.aspect;
-        float screenHeight = Camera.main.orthographicSize * 2;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    // Taille de l'écran en unités du monde.
+    //    float screenWidth = Camera.main.orthographicSize * 2 * Camera.main.aspect;
+    //    float screenHeight = Camera.main.orthographicSize * 2;
 
-        // Ajouter un Rigidbody2D pour bloquer la physique pendant le drag
-        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+    //    // Ajouter un Rigidbody2D pour bloquer la physique pendant le drag
+    //    Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
 
-    }
+    //}
 
-    void Update()
-    {
-        if (isDragging)
-        {
-            // Suivre la souris ou le toucher
-            Vector2 newPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) + mouseOffset;
-            transform.position = newPosition;
-            
-            
-            
-        }
-    }
+    //void Update()
+    //{
+    //    if (isDragging)
+    //    {
+    //        // Suivre la souris ou le toucher
+    //        Vector2 newPosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) + mouseOffset;
+    //        transform.position = newPosition;
+
+
+
+    //    }
+    //}
 
     void OnMouseDown()
     {
-        if (!isDragging)
-        {
-            // Sauvegarde l'offset pour le drag
-            mouseOffset = (Vector2)transform.position - (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            isDragging = true;
-        }
+
+        Instantiate(BallsPrefabs);
+
     }
 
-    void OnMouseUp()
-    {
-        if (isDragging)
-        {
-            transform.position = transform.position;
-            isDragging = false;
+    //void OnMouseUp()
+    //{
+    //    if (isDragging)
+    //    {
+    //        transform.position = transform.position;
+    //        isDragging = false;
 
-        }
-    }
+    //    }
+    //}
 
     void OnSpaceDown()
     {
+
+        Instantiate(BallsPrefabs);
 
     }
 
